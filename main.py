84d01affbc40b552edee3e1beaa967e7110cd1bc
Franklin Amador <joelamador55@gmail.com>
2024-10-21 22:10:37 -0600

    cors solucionado

--- main.py
+++ main.py
@@ -4,15 +4,11 @@ from routes import personas_routes, auth_routes
 
 app = FastAPI()
 
-# Configuración de CORS para permitir que el frontend acceda al backend
-origins = [
-    "http://localhost:3000",  # Añadir el dominio del frontend (en este caso Next.js)
-    "https://your-frontend-domain.com"
-]
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
