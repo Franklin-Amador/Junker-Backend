70158826bd9ada1fc13ee790fba33fb138c5a0e2
Franklin Amador <joelamador55@gmail.com>
2024-10-25 19:48:17 -0600

    middleware

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # * para permitir cualquier origen 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
